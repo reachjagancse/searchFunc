4b4420f93f0989d72d2b0046a36ccd6d8e180381
Jagaveerapandian Madhaiyan <mpandiancse@gmail.com>
2018-12-10 09:41:02 +0530

    Update searchjs.js

--- searchjs.js
+++ searchjs.js
@@ -1,91 +1,16 @@
-function searchAndHighlight(searchTerm, selector) {
-    if (searchTerm) {
-        //var wholeWordOnly = new RegExp("\\g"+searchTerm+"\\g","ig"); //matches whole word only
-        //var anyCharacter = new RegExp("\\g["+searchTerm+"]\\g","ig"); //matches any word with any of search chars characters
-        var selector = selector || "#realTimeContents"; //use body as selector if none provided
-        var searchTermRegEx = new RegExp(searchTerm, "ig");
-        var matches = $(selector).text().match(searchTermRegEx);
-        if (matches != null && matches.length > 0) {
-            $('.highlighted').removeClass('highlighted'); //Remove old search highlights  
-
-            //Remove the previous matches
-            $span = $('#realTimeContents span');
-            $span.replaceWith($span.html());
-
-    if (searchTerm === "&") {
-        searchTerm = "&amp;";
-        searchTermRegEx = new RegExp(searchTerm, "ig");
-    }
-            $(selector).html($(selector).html().replace(searchTermRegEx, "<span class='match'>" + searchTerm + "</span>"));
-            $('.match:first').addClass('highlighted');
-
-            var i = 0;
-
-            $('.next_h').off('click').on('click', function () {
-                i++;
-
-                if (i >= $('.match').length) i = 0;
-
-                $('.match').removeClass('highlighted');
-                $('.match').eq(i).addClass('highlighted');
-                $('.ui-mobile-viewport').animate({
-                    scrollTop: $('.match').eq(i).offset().top
-                }, 300);
-            });
-            $('.previous_h').off('click').on('click', function () {
-
-                i--;
-
-                if (i < 0) i = $('.match').length - 1;
-
-                $('.match').removeClass('highlighted');
-                $('.match').eq(i).addClass('highlighted');
-                $('.ui-mobile-viewport').animate({
-                    scrollTop: $('.match').eq(i).offset().top
-                }, 300);
-            });
+var inputText = document.getElementById("inputText");
+  var innerHTML = inputText.innerHTML;
+  var index = innerHTML.indexOf(text);
+  if (index >= 0) { 
+   innerHTML = innerHTML.substring(0,index) + "<span class='highlight'>" + innerHTML.substring(index,index+text.length) + "</span>" + innerHTML.substring(index + text.length);
+   inputText.innerHTML = innerHTML;
+  }
 
 
-
-
-            if ($('.highlighted:first').length) { //if match found, scroll to where the first one appears
-                $(window).scrollTop($('.highlighted:first').position().top);
-            }
-            return true;
-        }
-    }
-    return false;
+.highlight {
+  background-color: yellow;
 }
 
-$(document).on('click', '.searchButtonClickText_h', function (event) {
-
-    $(".highlighted").removeClass("highlighted").removeClass("match");
-    if (!searchAndHighlight($('.textSearchvalue_h').val())) {
-        alert("No results found");
-    }
-
-
-});
-
-
-$(this).append(
-    $('<input>', {
-        type: 'text',
-        val: $('#div1').text()
-    })
-);
-
-$(function(){
-  $("#div").bind("touchstart", function (event) {
-    alert(event.touches.length);
-  });
-});
-
-  var container = document.getElementById("container");
-
- var input = document.createElement("input");
-                input.type = "text";
-                input.name = "member" + i;
-                container.appendChild(input);
-                // Append a line break 
-                container.appendChild(document.createElement("br"));
+newTH.addEventListener( 'click', function(){
+  // delete the column here
+} );
